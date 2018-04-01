64b1d7e9c0db4ef63f8d31b7ae9ef3ea5730cdbe
Kevin Liu <kevinliu314@gmail.com>
2018-04-01 15:13:05 -0500

    finish part one single variate gradiant decsent

--- ML_Coursera/P1/ex1/gradientDescent.m
+++ ML_Coursera/P1/ex1/gradientDescent.m
@@ -6,7 +6,8 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
+thetalen = length(theta)
+d=theta
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,9 +17,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
+	for i = 1:thetalen
+		d(i,1) = sum(((X*theta)-y).*X(:,i));
+	end
+	theta = theta - (alpha/m)*d;
 
 
 
