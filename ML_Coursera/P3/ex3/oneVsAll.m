a91e4c331909d7c9d347e20747a7db970950045b
Kevin Liu <kevinliu314@gmail.com>
2018-04-24 20:40:56 -0500

    one vs all classifier training
    will have to review this section. was able to follow the directions but
    i need to understand it more

--- ML_Coursera/P3/ex3/oneVsAll.m
+++ ML_Coursera/P3/ex3/oneVsAll.m
@@ -49,14 +49,13 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+all_theta = zeros(num_labels, n+1);
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
-
-
-
-
-
-
-
+for c = 1 : num_labels
+	theta = all_theta(c,:)';
+	all_theta(c,:) = fmincg(@(t)(lrCostFunction(t,X,(y==c),lambda)),theta,options);
+end
 
 
 
